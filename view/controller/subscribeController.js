be844e2d2de9591460165e80e3000ed4ae6ac579
henrietta jeansson <henriettajeansson@gmail.com>
2024-05-13 13:10:18 +0200

    add view mvc and some html and json subscription data

---
+++ view/controller/subscribeController.js
@@ -0,0 +1,9 @@
+/** @format */
+
+function subscribe(plan) {
+  // Handle subscription process here, such as calling a PHP script to create a subscription via Stripe API
+}
+
+function selectStart(id) {}
+function selectMedium(id) {}
+function selectAdvanced(id) {}
--- view/subscriptionView.php
+++ view/subscriptionView.php
@@ -1,16 +1,38 @@
 <?php
 include '/partials/header.php';
+
+$json = file_get_contents('/model/subscriptionPlans.json');
+$json_data = json_decode($json, true);
+// print_r($json_data);
 ?>
 
 <h2>Subscription Plans/h2>
-    <?php
-    // $subscriptions = SubscriptionModel::getSubscriptions();
 
-    // $subscriptions = null;
-
-    // if (isset($_GET['subscription-plans'])) {
-
-    //     $subscriptions = SubscriptionModel::getSubscriptions();
-    // }
+    <table>
+        <tr>
+            <th>Plan</th>
+            <th>Features</th>
+            <th>Price</th>
+            <th>Subscribe</th>
+        </tr>
+        <tr>
+            <td>Basic</td>
+            <td>Feature 1, Feature 2</td>
+            <td>$10/month</td>
+            <td><button onclick="subscribe('basic')">Subscribe</button></td>
+        </tr>
+        <tr>
+            <td>Medium</td>
+            <td>Feature 1, Feature 2, Feature 3</td>
+            <td>$20/month</td>
+            <td><button onclick="subscribe('medium')">Subscribe</button></td>
+        </tr>
+        <tr>
+            <td>Premium</td>
+            <td>Feature 1, Feature 2, Feature 3, Feature 4</td>
+            <td>$30/month</td>
+            <td><button onclick="subscribe('premium')">Subscribe</button></td>
+        </tr>
+    </table>
     include '/partials/footer.php';
     ?>
